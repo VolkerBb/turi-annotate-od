82804ffe35da22ac4ff53e9e87650977fb56981c
Volker Bublitz <Volker.Bublitz@t-systems.com>
2018-09-22 16:43:53 +0200

    Initial Commit

---
+++ ImagePreparation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImagePreparation
+//
+//  Created by Volker Bublitz on 22.09.18.
+//  Copyright © 2018 vobu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
